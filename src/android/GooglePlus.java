0e3a019e2a1af12e204c1d37b5132032e17af634
sithjobs <mrsithjobs@gmail.com>
2018-02-03 17:58:12 -0800

    need all info

--- src/android/GooglePlus.java
+++ src/android/GooglePlus.java
@@ -356,6 +356,7 @@ public class GooglePlus extends CordovaPlugin implements GoogleApiClient.OnConne
                         result.put("familyName", acct.getFamilyName());
                         result.put("givenName", acct.getGivenName());
                         result.put("imageUrl", acct.getPhotoUrl());
+                        result.put("Data", acct);
                         savedCallbackContext.success(result);
                     } catch (Exception e) {
                         savedCallbackContext.error("Trouble obtaining result, error: " + e.getMessage());
